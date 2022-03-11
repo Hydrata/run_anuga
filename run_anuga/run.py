04dea8b20c14120814e3a712a912b14f9d35248f
davekennewell <davekennewell@gmail.com>
2022-03-11 10:57:24 +0400

    cleanup and parse boundary_tags properly

---
+++ run_anuga/run.py
@@ -0,0 +1,167 @@
+import anuga
+import argparse
+import logging
+import math
+import os
+import pandas as pd
+import traceback
+
+from osgeo import gdal
+
+from anuga import distribute, finalize, barrier
+from anuga.utilities import quantity_setting_functions as qs
+from anuga.utilities import plot_utils as util
+from anuga.operators.rate_operators import Polygonal_rate_operator
+from run_anuga.utils import is_dir_check, setup_input_data, update_web_interface, create_boundary_polygon_from_boundaries
+
+logger = logging.getLogger(__name__)
+
+
+def run_sim(package_dir, username=None, password=None):
+    run_args = package_dir, username, password
+    input_data = setup_input_data(package_dir)
+    output_stats = dict()
+    try:
+        if anuga.myid == 0:
+            update_web_interface(run_args, data={'status': 'building mesh'})
+            anuga.pmesh.mesh_interface.create_mesh_from_regions(
+                bounding_polygon=input_data['boundary_polygon'],
+                boundary_tags=input_data['boundary_tags'],
+                maximum_triangle_area=input_data['scenario_config'].get('maximum_triangle_area'),
+                interior_regions=[],
+                interior_holes=[],
+                filename=input_data['mesh_filepath'],
+                use_cache=False,
+                verbose=True
+            )
+            print(input_data['mesh_filepath'])
+            domain = anuga.shallow_water.shallow_water_domain.Domain(
+                mesh_filename=input_data['mesh_filepath'],
+                use_cache=False,
+                verbose=True,
+            )
+            domain.set_name(input_data['run_label'])
+            domain.set_datadir(input_data['output_directory'])
+            domain.set_minimum_storable_height(0.005)
+
+            poly_fun_pairs = [['Extent', input_data['elevation_filename']]]
+            elevation_function = qs.composite_quantity_setting_function(
+                poly_fun_pairs,
+                domain,
+                nan_treatment='exception',
+            )
+            domain.set_quantity('elevation', elevation_function, verbose=True, alpha=0.99)
+            domain.set_quantity('friction', 0.06, verbose=True)
+            domain.set_quantity('stage', 0.0, verbose=True)
+
+            update_web_interface(run_args, data={'status': 'created mesh'})
+        else:
+            domain = None
+        barrier()
+        domain = distribute(domain, verbose=True)
+        domain.set_boundary({
+                'exterior': anuga.Dirichlet_boundary([0, 0, 0]),
+                'interior': anuga.Reflective_boundary(domain),
+                'Dirichlet': anuga.Dirichlet_boundary([0, 0, 0]),
+                'Reflective': anuga.Reflective_boundary(domain),
+                'Transmissive': anuga.Transmissive_boundary(domain)
+            })
+
+        # setup rainfall
+        def rain(time_in_seconds):
+            t_sec = int(math.floor(time_in_seconds))
+            return rain_df['rate_m_s'][t_sec]
+
+        duration = input_data['scenario_config'].get('duration')
+        constant_rainfall = input_data['scenario_config'].get('constant_rainfall') or 100
+        date_rng = pd.date_range(start='1/1/2022', periods=duration + 1, freq='s')
+        rain_df = pd.DataFrame(date_rng, columns=['datetime'])
+        rain_df['rate_m_s'] = constant_rainfall / 1000
+        Polygonal_rate_operator(domain, rate=rain, factor=1.0e-3, polygon=input_data['boundary_polygon'], default_rate=0.00)
+
+        for t in domain.evolve(yieldstep=60, finaltime=duration):
+            domain.write_time()
+            print(f'domain.evolve {t} on processor {anuga.myid}')
+            update_web_interface(run_args, data={"status": f"{round(t/duration * 100, 0)}%"}) if anuga.myid == 0 else None
+            print(domain.timestepping_statistics())
+        domain.sww_merge(verbose=True, delete_old=True)
+        barrier()
+
+        if anuga.myid == 0:
+            print('Generating output rasters...')
+            raster = gdal.Open(input_data['elevation_filename'])
+            gt = raster.GetGeoTransform()
+            resolution = 1 if math.floor(gt[1] / 4) == 0 else math.floor(gt[1] / 4)
+            epsg_integer = int(input_data['scenario_config'].get("epsg").split(":")[1]
+                               if ":" in input_data['scenario_config'].get("epsg")
+                               else input_data['scenario_config'].get("epsg"))
+            util.Make_Geotif(
+                swwFile=f"{input_data['output_directory']}/{input_data['run_label']}.sww",
+                output_quantities=['depth', 'velocity', 'depthIntegratedVelocity'],
+                myTimeStep='max',
+                CellSize=resolution,
+                lower_left=None,
+                upper_right=None,
+                EPSG_CODE=epsg_integer,
+                proj4string=None,
+                velocity_extrapolation=True,
+                min_allowed_height=1.0e-05,
+                output_dir=input_data['output_directory'],
+                bounding_polygon=input_data['boundary_polygon'],
+                internal_holes=None,
+                verbose=False,
+                k_nearest_neighbours=3,
+                creation_options=[]
+            )
+            logger.info('Successfully generated depth, velocity, momentum outputs')
+            update_web_interface(
+                run_args,
+                data={"status": "uploading depth_max"},
+                files={
+                    "tif_depth_max": open(
+                        f"{input_data['output_directory']}/{input_data['run_label']}_depth_max.tif",
+                        'rb'
+                    )
+                }
+            )
+            update_web_interface(
+                run_args,
+                data={"status": "uploading depthIntegratedVelocity_max"},
+                files={
+                    "tif_depth_integrated_velocity_max": open(
+                        f"{input_data['output_directory']}/{input_data['run_label']}_depthIntegratedVelocity_max.tif",
+                        'rb'
+                    )
+                }
+            )
+            update_web_interface(
+                run_args,
+                data={"status": "uploading velocity_max"},
+                files={
+                    "tif_velocity_max": open(
+                        f"{input_data['output_directory']}/{input_data['run_label']}_velocity_max.tif",
+                        'rb'
+                    )
+                }
+            )
+            logger.info('Successfully uploaded outputs')
+    except Exception as e:
+        logger.error(traceback.format_exc())
+    finally:
+        barrier()
+        finalize()
+    return f"finished: {input_data['run_label']}"
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("username", nargs='?', help="your username(email) at hydrata.com", type=str)
+    parser.add_argument("password", nargs='?', help="your password at hydrata.com", type=str)
+    parser.add_argument("--package_dir", "-wd", help="the base directory for your simulation, it contains the scenario.json file", type=is_dir_check)
+    args = parser.parse_args()
+    username = args.username
+    password = args.password
+    package_dir = args.package_dir
+    if not package_dir:
+        package_dir = os.getcwd()
+    run_sim(package_dir, username, password)
---
+++ run_anuga/utils.py
@@ -0,0 +1,138 @@
+import argparse
+import math
+import json
+import logging
+import os
+import requests
+
+from pathlib import Path
+from osgeo import ogr
+
+logger = logging.getLogger(__name__)
+
+
+def is_dir_check(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
+
+
+def setup_input_data(package_dir):
+    if not os.path.isfile(os.path.join(package_dir, 'scenario.json')):
+        raise FileNotFoundError(f'Could not find "scenario.json" in {package_dir}')
+
+    input_data = dict()
+    input_data['scenario_config'] = json.load(open(os.path.join(package_dir, 'scenario.json')))
+    project_id = input_data['scenario_config'].get('project')
+    scenario_id = input_data['scenario_config'].get('id')
+    run_id = input_data['scenario_config'].get('run_id')
+    input_data['run_label'] = f"run_{project_id}_{scenario_id}_{run_id}"
+    input_data['output_directory'] = os.path.join(package_dir, f'outputs_{project_id}_{scenario_id}_{run_id}')
+    input_data['mesh_filepath'] = f"{input_data['output_directory']}/run_{scenario_id}_{run_id}.msh"
+    Path(input_data['output_directory']).mkdir(parents=True, exist_ok=True)
+
+    input_data['boundaries'] = json.load(
+        open(os.path.join(package_dir, f"inputs/{input_data['scenario_config'].get('boundary')}"))
+    )
+    input_data['friction_maps'] = json.load(
+        open(os.path.join(package_dir, f"inputs/{input_data['scenario_config'].get('friction')}"))
+    )
+    input_data['inflows'] = json.load(
+        open(os.path.join(package_dir, f"inputs/{input_data['scenario_config'].get('inflow')}"))
+    )
+    input_data['structures'] = json.load(
+        open(os.path.join(package_dir, f"inputs/{input_data['scenario_config'].get('structure')}"))
+    )
+    input_data['mesh_regions'] = json.load(
+        open(os.path.join(package_dir, f"inputs/{input_data['scenario_config'].get('mesh_region')}"))
+    )
+    input_data['elevation_filename'] = os.path.join(package_dir, f"inputs/{input_data['scenario_config'].get('elevation')}")
+
+    boundary_polygon, boundary_tags = create_boundary_polygon_from_boundaries(input_data['boundaries'])
+    input_data['boundary_polygon'] = boundary_polygon
+    input_data['boundary_tags'] = boundary_tags
+
+    logger.debug(f"{input_data['boundaries']=}")
+    logger.debug(f"{input_data['friction_maps']=}")
+    logger.debug(f"{input_data['inflows']=}")
+    logger.debug(f"{input_data['structures']=}")
+    logger.debug(f"{input_data['mesh_regions']=}")
+    logger.debug(f"{input_data['elevation_filename']=}")
+    return input_data
+
+
+def update_web_interface(run_args, data, files):
+    package_dir, username, password = run_args
+    if username and password:
+        input_data = setup_input_data(package_dir)
+        data['project'] = input_data['scenario_config'].get('project')
+        data['scenario'] = input_data['scenario_config'].get('id')
+        run_id = input_data['scenario_config'].get('run_id')
+        client = requests.Session()
+        client.auth = requests.auth.HTTPBasicAuth(username, password)
+        response = client.patch(
+            f"https://hydrata.com/anuga/api/{data['project']}/{data['scenario']}/run/{run_id}/",
+            data=data,
+            files=files
+        )
+        logger.info(response.status_code, response.content)
+
+
+def create_boundary_polygon_from_boundaries(boundaries_geojson):
+    geometry_collection = ogr.Geometry(ogr.wkbGeometryCollection)
+    # Create a dict of the available boundary tags
+    boundary_tags = dict()
+    all_x_coordinates = list()
+    all_y_coordinates = list()
+    for index, feature in enumerate(boundaries_geojson.get('features')):
+        if feature.get('properties').get('location') != "External":
+            continue
+        boundary_tags[feature.get('properties').get('boundary')] = []
+        geometry = ogr.CreateGeometryFromJson(json.dumps(feature.get('geometry')))
+        geometry_collection.AddGeometry(geometry)
+        # Collect a list of the coordinates associated with each boundary tag:
+        feature_coordinates = feature.get('geometry').get('coordinates')
+        for coordinate in feature_coordinates:
+            all_x_coordinates.append(coordinate[0])
+            all_y_coordinates.append(coordinate[1])
+
+    # Find the center of our project, the sort the boundary lines in clockwise direction around it
+    max_x = max(all_x_coordinates)
+    max_y = max(all_y_coordinates)
+    min_x = min(all_x_coordinates)
+    min_y = min(all_y_coordinates)
+    mid_x = max_x - (max_x - min_x) / 2
+    mid_y = max_y - (max_y - min_y) / 2
+    line_list = list()
+    for index, feature in enumerate(boundaries_geojson.get('features')):
+        if feature.get('properties').get('location') != "External":
+            continue
+        geometry = ogr.CreateGeometryFromJson(json.dumps(feature.get('geometry')))
+        centroid = json.loads(geometry.Centroid().ExportToJson()).get('coordinates')
+        line_list.append({
+            "centroid": centroid,
+            "boundary": feature.get('properties').get('boundary'),
+            "id": feature.get('id'),
+            "angle": math.atan((centroid[0] - mid_x)/(centroid[1] - mid_y)),
+            "coordinates": feature.get('geometry').get('coordinates')
+        })
+    line_list.sort(key=lambda line: line.get('angle'))
+
+    # Now join all our lines in clockwise order
+    boundary_polygon = list()
+    for index, line in enumerate(line_list):
+        boundary_polygon.extend(line.get("coordinates"))
+        boundary_tags[line.get("boundary")].append(index)
+
+    # Save a GeoJson copy so we can debug/test what happened, if it's not working for any reason
+    ring = ogr.Geometry(ogr.wkbLinearRing)
+    for coordinate in boundary_polygon:
+        ring.AddPoint(coordinate[0], coordinate[1])
+    geojson = ring.ExportToJson()
+    filepath = os.path.join(os.getcwd(), 'test.geojson')
+    print(filepath)
+    with open(filepath, 'w', encoding='utf-8') as f:
+        json.dump(json.loads(geojson), f, ensure_ascii=False, indent=4)
+
+    return boundary_polygon, boundary_tags
