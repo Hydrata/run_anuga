6c752d45a4dc158380ceba8cbf1e21452a67aacc
davekennewell <davekennewell@gmail.com>
2022-05-20 14:40:44 +0400

    don't require mesh_region

--- run_anuga/run.py
+++ run_anuga/run.py
@@ -131,9 +131,10 @@ def run_sim(package_dir, username=None, password=None):
             gt = raster.GetGeoTransform()
             resolution = 1 if math.floor(gt[1] / 4) == 0 else math.floor(gt[1] / 4)
             resolutions = list()
-            for feature in input_data.get('mesh_region').get('features') or list():
-                logger.info(f'{feature=}')
-                resolutions.append(feature.get('properties').get('resolution'))
+            if input_data.get('mesh_region'):
+                for feature in input_data.get('mesh_region').get('features') or list():
+                    logger.info(f'{feature=}')
+                    resolutions.append(feature.get('properties').get('resolution'))
             logger.info(f'{resolutions=}')
             if len(resolutions) == 0:
                 resolutions = [1000]
