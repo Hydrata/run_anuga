2f566e8147d93f1016b8d0fd544b0d3525d188c4
Dave Kennewell <david.kennewell@hydrata.com>
2024-06-26 16:28:06 +0400

    move merged sww file out of batch_output_directory

--- run_anuga/run.py
+++ run_anuga/run.py
@@ -1,4 +1,7 @@
+import glob
 import json
+import shutil
+
 import dill as pickle
 import time
 import numpy
@@ -27,7 +30,7 @@ def run_sim(package_dir, username=None, password=None, batch_number=1):
     input_data = setup_input_data(package_dir)
     logger = setup_logger(input_data, username, password, batch_number)
     logger.critical(f"run_sim started with {batch_number=}")
-    output_directory = f"{input_data['output_directory']}_{batch_number}"
+    batch_output_directory = f"{input_data['output_directory']}_{batch_number}"
     domain = None
     overall = None
     sim_success = True
@@ -100,7 +103,7 @@ def run_sim(package_dir, username=None, password=None, batch_number=1):
                     break
             if not overall:
                 raise Exception("Unable to open checkpoint file")
-            domain.set_datadir(output_directory)
+            domain.set_datadir(batch_output_directory)
             domain.last_walltime = time.time()
             domain.set_name(input_data['run_label'])
             domain.communication_time = 0.0
@@ -152,7 +155,7 @@ def run_sim(package_dir, username=None, password=None, batch_number=1):
                         epsg_code=input_data['scenario_config'].get('epsg')
                     )
             domain.set_name(input_data['run_label'])
-            domain.set_datadir(output_directory)
+            domain.set_datadir(batch_output_directory)
             domain.set_minimum_storable_height(0.005)
             frictions = make_frictions(input_data)
             friction_function = qs.composite_quantity_setting_function(
@@ -270,6 +273,10 @@ def run_sim(package_dir, username=None, password=None, batch_number=1):
                 start = time.time()
         barrier()
         domain.sww_merge(verbose=True, delete_old=False)
+        # copy the sww out of the batch directory and into outputs
+        sww_files = glob.glob(f"{batch_output_directory}/*.sww")
+        merged_sww_file = max([(file, os.path.getsize(file)) for file in sww_files], key=lambda x: x[1])
+        shutil.move(merged_sww_file, input_data['output_directory'])
         barrier()
         if anuga.myid == 0:
             max_memory_usage = int(round(max(memory_usage_logs)))
